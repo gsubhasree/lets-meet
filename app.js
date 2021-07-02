f7f1814c3746d55c9c39558ae53a81b3eae6d5b2
SS-2020 <gsubhasree2@gmail.com>
2021-07-02 22:09:00 +0530

    [fix] : require path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const http = require('http')
+const path= require("path")
 var cors = require('cors')
 const bodyParser = require('body-parser')
 
